da36372f7213cb173c47513f8bd5b37cd57dd606
zeesial <119788945+zeesial@users.noreply.github.com>
2023-01-06 20:58:02 +0500

    Update zia.py

--- zia.py
+++ zia.py
@@ -360,7 +360,7 @@ def m1(ids,passlist):
                         fbrv = '0'
                         fban = 'FB4A'
                         fbpn = 'com.facebook.katana'
-                        ua = 'Davik/2.1.0 (Linux; U; Android 10.0.0; Techno Build/TECHNO) [FBAN/Katana-Android;FBAV/196.0.0.29.99;FBPN/com.facebook.katana;FBLC/th_TH;FBBV/135374479;FBCR/MOBILINK;FBMF/Techno;FBBD/Techno;FBDV/Techno;FBSV/10.0.0;FBCA/armeabi-v7a:armeabi;FBDM/{density=3.0,width=720,height=1280};FB_FW/1;]'
+                        ua = 'Davik/2.1.0 (Linux; U; Android 10.0.0; Oppo F9 Pro Build/8BFOHT) [FBAN/FB4A;FBAV/92.866.944.616;FBPN/com.facebook.katana;FBLC/en_US;FBBV/322216925;FBCR/null;FBMF/Oppo;FBBD/Oppo;FBDV/Oppo;FBSV/Oppo.0.0;FBCA/armeabi-v7a:armeabi;FBDM/{density=2.25,width=1080,height=1920};]'
                         random_seed = random.Random()
                         adid = str(''.join(random_seed.choices(string.hexdigits, k=16)))
                         device_id = str(uuid.uuid4())
